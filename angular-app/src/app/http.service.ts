93c3cdf173ef4a4150b9e0790a52aefc90d35ef1
Andrew Ignacio <andrew.ignacio1@gmail.com>
2018-12-10 15:27:42 -0500

    updated continued

--- angular-app/src/app/http.service.ts
+++ angular-app/src/app/http.service.ts
@@ -7,12 +7,21 @@ import { HttpClient } from '@angular/common/http';
 
 export class HttpService {
    constructor(private _http: HttpClient){
-       this.getTasks();
+
    }
    getTasks(){
-       // our http response is an Observable, store it in a variable
-       let tempObservable = this._http.get('/tasks');
-       // subscribe to the Observable and provide the code we would like to do with our data from the response
-       tempObservable.subscribe(data => console.log("Got our tasks!", data));
+       // // our http response is an Observable, store it in a variable
+       // let tempObservable = this._http.get('/tasks');
+       // // subscribe to the Observable and provide the code we would like to do with our data from the response
+       // tempObservable.subscribe(data => console.log("Got our tasks!", data));
+       return this._http.get('/tasks');
     }
+    getATask(){
+        // our http response is an Observable, store it in a variable
+        // let itsatask = this._http.get('/tasks/:id');
+        // // subscribe to the Observable and provide the code we would like to do with our data from the response
+        // itsatask.subscribe(data => console.log("Got our task!", data));
+
+        return this._http.get('/tasks/5c0821770ea2104c9cbd96c7');
+     }
 }
--- server/models/task.js
+++ server/models/task.js
@@ -3,7 +3,7 @@ console.log("inside of task.js");
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-    title: String,
+    title: {type: String, required: [true, "Name is required"]},
     description: {type: String, default: "default description"},
     completed: {type: Boolean, default: false},
 },{timestamps:true});
